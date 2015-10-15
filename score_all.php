df9de5bace9df88cdea3039fd1ce0958b4a54a19
exgphe <exgphe@icloud.com>
2015-10-15 20:09:20 +0800

    Finished UI of all score subpages

---
+++ score_all.php
@@ -0,0 +1,19 @@
+<?php require './header.php';?>
+<div data-role="page" data-title="所有成绩" id="page_score_all">
+      <div class="ui-content" role="main">
+      <?php include './column.php';?>
+      <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">     
+      <p>姓名:</p>
+      <p></p>
+      <p>学霸指数：</p>
+      <p>获得称号：</p>
+      <p></p>
+      <ul>
+        <li>科目1:</li>
+        <li>科目2:</li>
+        <li>科目3:</li>
+      </ul>
+    </div>
+      </div>
+      </div>
+    <?php require './footer.php';?>
---
+++ score_gpa.php
@@ -0,0 +1,15 @@
+<?php require './header.php';?>
+<div data-role="page" data-title="GPA计算" id="page_score_gpa">
+      <div class="ui-content" role="main">
+      <?php include './column.php';?>
+      <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">
+    <p>姓名</p>
+<p>根据（学校）规则，你的gpa计算结果为：</p>
+                 <p>xxx</p>
+                 <p>计算规则如下：</p>
+                 <p>xxx</p>
+</div>
+      </div>
+      
+      </div>
+    <?php require './footer.php';?>
