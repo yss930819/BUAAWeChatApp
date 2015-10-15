da9cb90f28fb9165c1b01f0f182a48160b2c5035
exgphe <exgphe@icloud.com>
2015-10-15 20:01:36 +0800

    Found a way to provide a better experience when exploring sub pages: Back button adds/dismisses automatically only loads the required div from the server

--- score.php
+++ score.php
@@ -1,5 +1,5 @@
 <?php require './header.php';?>
-    <div data-role="page" data-title="成绩">
+    <div data-role="page" data-title="成绩" id="page_score_main">
       <div class="ui-content" role="main">
       <?php include './column.php';?>
   <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">
@@ -8,14 +8,12 @@
 <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">
     <p>能力分析区域</p>
 </div>
-<a href="#" class="ui-btn ui-corner-all">GPA计算</a>
-<a href="#" class="ui-btn ui-corner-all">最新成绩</a>
-<a href="#" class="ui-btn ui-corner-all">所有成绩</a>
-<a href="#" class="ui-btn ui-corner-all">成绩记录本</a>
+<a href="score_gpa.php" class="ui-btn ui-corner-all navigate_to_subpage">GPA计算</a>
+<a href="score_latest.php" class="ui-btn ui-corner-all navigate_to_subpage">最新成绩</a>
+<a href="score_all.php" class="ui-btn ui-corner-all navigate_to_subpage">所有成绩</a>
+<a href="score_memos.php" class="ui-btn ui-corner-all navigate_to_subpage">成绩记录本</a>
       </div>
-      <!-- /content --> 
-      
-    </div>
+      </div>
     <!-- /page -->
-
+    
     <?php require './footer.php';?>
