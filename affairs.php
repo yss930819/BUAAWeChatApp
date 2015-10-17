b9a842a1e9bcc230eedb308a6b30b63de41a7bf8
exgphe <exgphe@icloud.com>
2015-10-17 21:13:32 +0800

    Finished UI of affairs.php

--- affairs.php
+++ affairs.php
@@ -2,6 +2,21 @@
 <div data-role="page" data-title="相关事">
   <div class="ui-content" role="main">
     <?php include './column.php';?>
+    <h2>校历日程</h2>
+    <div class="ui-grid-a">
+      <div class="ui-block-a" style="text-align:center">
+          <a href="#" data-role="button">每日签到</a>
+          <span>你已经连续签到xx天</span>
+      </div>
+      <div class="ui-block-b"><div class="ui-body ui-body-a ui-corner-all"><img src="http://images.apple.com/v/ios/m/images/overview/hero_large_2x.jpg" style="width: -webkit-fill-available"/></div></div>
+      <hr />
+      <div data-role="controlgroup">
+    <a href="#" class="ui-btn ui-corner-all">No icon</a>
+<a href="#" class="ui-btn ui-corner-all">No icon</a>
+<a href="#" class="ui-btn ui-corner-all">No icon</a>
+</div>
+    </div> 
+  </div>
   </div>
   <!-- /content --> 
   
