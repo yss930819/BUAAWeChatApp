440c3a8c2b17bdd0d45d5c359135dfa01542e0fe
peng <253147513@qq.com>
2015-10-21 22:07:04 +0800

    使用内联样式增大了底部导航栏的字号

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div data-role="footer" data-position="fixed" data-tap-toggle="false" data-theme="a">
 		<div data-role="navbar">
 			<ul>
-				<li><a href="score.php" data-prefetch="true" data-transition="fade">成绩</a></li>
-				<li><a href="syllabus.php" data-prefetch="true" data-transition="fade">课表</a></li>
-				<li><a href="affairs.php" data-prefetch="true" data-transition="fade">相关事</a></li>
+				<li><a href="score.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">成绩</a></li>
+				<li><a href="syllabus.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">课表</a></li>
+				<li><a href="affairs.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">相关事</a></li>
 			</ul>
 		</div><!-- /navbar -->
 	</div><!-- /footer -->
