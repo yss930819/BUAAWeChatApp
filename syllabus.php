5bb393783d46c08444a0e9c35272df35b4ae094d
peng <253147513@qq.com>
2015-10-29 20:58:45 +0800

    增加了课表table

--- syllabus.php
+++ syllabus.php
@@ -10,16 +10,38 @@
     <script>prepareSwiper();</script>
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">
+				<table data-role="table" id="syllabus-table-1" class="table-stroke syllabus-table">
+                    <thead>
+                        <tr>
+                          <th>时间</th>
+                          <th>8:00~9:45</th>
+                          <th>10:00~11:45</th>
+                          <th>14:00~15:45</th>
+                          <th>16:00~17:45</th>
+                          <th>18:00~19:45</th>
+                          <th>20:00~21:45</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th>课程</th>
+                          <td>数学分析(第1~17周 主M101)</td>
+                          <td>无</td>
+                          <td>无</td>
+                          <td>英语</td>
+                          <td>无</td>
+                          <td>滚去睡觉</td>
+                        </tr>
+                      </tbody>
+                </table>
+            </div>
             <div class="swiper-slide">Slide 2</div>
             <div class="swiper-slide">Slide 3</div>
             <div class="swiper-slide">Slide 4</div>
             <div class="swiper-slide">Slide 5</div>
             <div class="swiper-slide">Slide 6</div>
             <div class="swiper-slide">Slide 7</div>
-            <div class="swiper-slide">Slide 8</div>
-            <div class="swiper-slide">Slide 9</div>
-            <div class="swiper-slide">Slide 10</div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
