038b579e9346cb75574476b0ff80eef3bb1ae891
exgphe <exgphe@icloud.com>
2015-11-01 16:36:30 +0800

    Introducing the whole new indigo theme according to the design images

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-<div data-role="footer" data-position="fixed" data-tap-toggle="false" data-theme="a">
-		<div data-role="navbar">
+<div data-role="footer" id="footer" data-position="fixed" data-tap-toggle="false" data-theme="a">
+		<div data-role="navbar" id="navbar">
 			<ul>
-				<li><a href="score.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">成绩</a></li>
-				<li><a href="syllabus.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">课表</a></li>
-				<li><a href="affairs.php" data-prefetch="true" data-transition="fade" style="font-size: 14px">相关事</a></li>
+				<li><a href="score.php" data-theme="c" data-prefetch="true" data-transition="fade" class="ui-corner-all">成绩</a></li>
+				<li><a href="syllabus.php" data-theme="c" data-prefetch="true" data-transition="fade" class="ui-corner-all">课表</a></li>
+				<li><a href="affairs.php" data-theme="c" data-prefetch="true" data-transition="fade" class="ui-corner-all">相关事</a></li>
 			</ul>
 		</div>
 	</div>
--- header.php
+++ header.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
-	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>北航微教务 - by Pride 1952 @ BUAA</title>
-	<link rel="shortcut icon" href="/favicon.ico">
-    <link rel="stylesheet" href="http://fonts.lug.ustc.edu.cn/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="/css/jquery.mobile-1.4.5.min.css">
-    <link rel="stylesheet" href="/css/main.css">
-	<script src="/js/jquery-2.1.4.min.js"></script>
-	<script src="/js/jquery.mobile-1.4.5.min.js"></script>
-    <script src="/js/main.js"></script>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>北航微教务 - by Pride 1952 @ BUAA</title>
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="stylesheet" href="http://fonts.lug.ustc.edu.cn/css?family=Open+Sans:300,400,700">
+<link rel="stylesheet" href="/css/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="/css/indigo.0.3.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<script src="/js/jquery-2.1.4.min.js"></script>
+<script src="/js/jquery.mobile-1.4.5.min.js"></script>
+<script src="/js/main.js"></script>
 </head>
 <body>
-    <div data-role="header" data-position="fixed" data-theme="a">
-		<a id="back_button" href="#" data-rel="back" class="ui-btn ui-btn-left ui-alt-icon ui-nodisc-icon ui-corner-all ui-btn-icon-notext ui-icon-carat-l">Back</a>
-        <h1>title</h1>
-    </div>
+<div data-role="header" data-position="fixed" data-theme="a"> <a id="back_button" href="#" data-rel="back" class="ui-btn ui-btn-left ui-nodisc-icon ui-corner-all ui-btn-icon-notext ui-icon-carat-l">Back</a>
+  <h1>title</h1>
+</div>
