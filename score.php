e18f3a63776d93abef607b59ae9baaaa0d714af0
exgphe <exgphe@icloud.com>
2015-11-01 15:15:37 +0800

    Minor change

--- score.php
+++ score.php
@@ -9,8 +9,7 @@
     <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">     
       <p>能力分析区域</p>
     </div>
-    <a href="score_gpa.php" class="ui-btn ui-corner-all">GPA计算</a> <a href="score_latest.php" class="ui-btn ui-corner-all">最新成绩</a> <a href="score_all.php" class="ui-btn ui-corner-all">所有成绩</a> <a href="score_memos.php" class="ui-btn ui-corner-all">成绩记录本</a> </div>
+    <div data-role="controlgroup"> <a href="score_gpa.php" class="ui-btn ui-corner-all">GPA计算</a> <a href="score_latest.php" class="ui-btn ui-corner-all">最新成绩</a> <a href="score_all.php" class="ui-btn ui-corner-all">所有成绩</a> <a href="score_memos.php" class="ui-btn ui-corner-all">成绩记录本</a></div>
+  </div>
 </div>
-
-
 <?php require './footer.php';?>
