0471c1631727e9066579f3c100e387dcc4323325
exgphe <exgphe@icloud.com>
2015-11-04 21:57:23 +0800

    Furnished UI of score_gpa.php

--- score_gpa.php
+++ score_gpa.php
@@ -1,15 +1,30 @@
 <?php require './header.php';?>
-<div data-role="page" data-title="GPA计算" id="page_score_gpa">
-  <div class="ui-content" role="main">
-    <?php include './column.php';?>
-    <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">     
-      <p>姓名</p>
-      <p>根据（学校）规则，你的gpa计算结果为：</p>
-      <p>xxx</p>
-      <p>计算规则如下：</p>
-      <p>xxx</p>
-    </div>
-    <div class="caption center">@Developed by Pride1952</div>
-  </div>
-</div>
-<?php require './footer.php';?>
+<html>
+ <head></head>
+ <body>
+  <div data-role="page" data-title="GPA计算" id="page_score_gpa"> 
+   <div class="ui-content" role="main"> 
+    <?php include './column.php';?> 
+    <div data-role="fieldcontain" class="ui-body ui-body-a center">
+     <h2>姓名：XXX</h2> 
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <p>根据（北京航空航天大学）规则</p> 
+     <p>你的GPA计算结果为：</p> 
+     <p>xxxx</p> 
+     <p>计算规则如下：</p> 
+     <p>xxxxxxxxx</p> 
+     <p>xxxxxxxxx</p> 
+     <p>xxxxxxxx</p> 
+    </div> 
+    <div class="caption center">
+     @Developed by Pride1952
+    </div> 
+   </div> 
+  </div> 
+  <?php require './footer.php';?>
+ </body>
+</html>
