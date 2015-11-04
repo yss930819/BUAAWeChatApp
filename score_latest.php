248dea4b221f934fd44ce5e335d0596f5306dcfc
exgphe <exgphe@icloud.com>
2015-11-04 22:03:41 +0800

    Furnished UI of score_latest.php

--- score_latest.php
+++ score_latest.php
@@ -1,21 +1,30 @@
 <?php require './header.php';?>
-<div data-role="page" data-title="最新成绩" id="page_score_latest">
-  <div class="ui-content" role="main">
-    <?php include './column.php';?>
-    <div data-role="fieldcontain" class="ui-body ui-body-a ui-corner-all">     
-      <p>姓名:</p>
-      <p>在（时间）内，你的</p>
-      <p>学霸指数：</p>
-      <p>获得称号：</p>
-      <p></p>
-      <ul>
-        <li>科目1:</li>
-        <li>科目2:</li>
-        <li>科目3:</li>
-      </ul>
-    </div>
-    <div class="caption center">@Developed by Pride1952</div>
-  </div>
-</div>
-</div>
-<?php require './footer.php';?>
+<html>
+ <head></head>
+ <body>
+  <div data-role="page" data-title="最新成绩" id="page_score_latest"> 
+   <div class="ui-content" role="main"> 
+    <?php include './column.php';?> 
+    <div data-role="fieldcontain" class="ui-body ui-body-a center">
+     <h2>姓名:</h2> 
+     <br /> 
+     <br /> 
+     <br /> 
+     <p>在（2015.09~2016.01）内，</p> 
+     <p>你的学霸指数：</p> 
+     <h3>★★★★★★</h3> 
+     <p>获得称号：</p> 
+     <ul style="text-align:left;list-style-type:none;"> 
+      <li>科目1:xxxxxxxxxxx</li> 
+      <li>科目2:xxxxx</li> 
+      <li>科目3:xxxxxxxx</li> 
+     </ul> 
+    </div> 
+    <div class="caption center">
+     @Developed by Pride1952
+    </div> 
+   </div> 
+  </div>  
+  <?php require './footer.php';?>
+ </body>
+</html>
