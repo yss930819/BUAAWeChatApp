b129b9242c02ed338ff4afad2c01fc8e8460ce65
exgphe <exgphe@icloud.com>
2015-11-04 13:49:47 +0800

    Finished UI of newuser.php

--- newuser.php
+++ newuser.php
@@ -1,27 +1,29 @@
-<?php require './header.php';?>
-<div data-role="page" data-title="登录" id="newuser-page">
-  <div class="ui-content" role="main">
-    <?php include './column.php';?>
-    
-    <div class="caption center">
-      <img src="/img/placeholder.png" style="width:30%" class="center" /><p>@Developed by Pride1952</p>
-      </div>
-    <div class="ui-body ui-body-c">           
-      <div data-role="fieldcontain">
-        <input data-role="none" type="text" name="username" id="username" placeholder="用户名" value=""  />
-        <hr />
-        <input data-role="none" type="password" name="password" id="password" placeholder="密码" value=""  />
-        
-      </div>
-      </div>
-      <input type="submit" value="登录" />
-      <h5>注意事项:</h5>
-      <ol data-role="listview">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ol>
-            
-  </div>
-</div>
-<?php require './footer.php';?>
+<!--?php require './header.php';?-->
+<html>
+ <head></head>
+ <body>
+  <div data-role="page" data-title="登录教务系统" id="newuser-page"> 
+   <div class="ui-content" role="main"> 
+    <!--?php include './column.php';?--> 
+    <div class="caption center"> 
+     <img src="/img/placeholder.png" style="width:30%" class="center" />
+     <p>@Developed by Pride1952</p> 
+    </div> 
+    <div class="ui-body ui-body-c">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+     <div data-role="fieldcontain"> 
+      <input data-role="none" type="text" name="username" id="username" placeholder="用户名" value="" /> 
+      <hr /> 
+      <input data-role="none" type="password" name="password" id="password" placeholder="密码" value="" /> 
+     </div> 
+    </div> 
+    <input type="submit" value="登录" /> 
+    <h5>注意事项:</h5> 
+    <h6>1.请输入您的学校教务用户名及密码</h6> 
+    <h6>2.区分大小写</h6> 
+    <h6>3.xxxxxxxxx</h6> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+   </div> 
+  </div> 
+  <!--?php require './footer.php';?-->
+ </body>
+</html>
