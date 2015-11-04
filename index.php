a379acd4c21fd7de0e0dfa4faf2e49beb4cda23c
exgphe <exgphe@icloud.com>
2015-11-04 13:18:47 +0800

    1.Furnished the look of index.php 2.Make a borderless and marginless ui-body

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php require './header.php';?>
-<div data-role="page" data-title="欢迎" class="jqm-demos">
+<div data-role="page" data-title="北航微教务" id="index-page">
   <div role="main" class="ui-content jqm-fullwidth">
     <?php include './column.php';?>
-    <h1>欢迎回来，XXX</h1>
-    <h2>此处为空白的文档，为后期可以自由添加内容的模块（既有图片，也可以是文字）</h2>
-    <h3>（既有图片，也可以是文字）</h3>
+    <div class="ui-body ui-body-a center">
+    <h1>欢迎回来：XXX</h1>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <p>此处为空白的文档，为后期可以自由添加内容的模块（既有图片，也可以是文字）</p>
+    <br />
+    <br />
+    <p>（既有图片，也可以是文字）</p>
+    </div>
   </div>
   <div class="caption center">@Developed by Pride1952</div>
 </div>
