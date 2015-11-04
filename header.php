1c79834120e9a00851d286c15c6f61e7adf97ede
exgphe <exgphe@icloud.com>
2015-11-04 12:56:04 +0800

    Introduce the theme indigo.0.4 which is closer to the design chart

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="stylesheet" href="http://fonts.lug.ustc.edu.cn/css?family=Open+Sans:300,400,700">
 <link rel="stylesheet" href="/css/jquery.mobile-1.4.5.min.css">
-<link rel="stylesheet" href="/css/indigo.0.3.min.css">
+<link rel="stylesheet" href="/css/indigo.0.4.min.css">
 <link rel="stylesheet" href="/css/main.css">
 <script src="/js/jquery-2.1.4.min.js"></script>
 <script src="/js/jquery.mobile-1.4.5.min.js"></script>
--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <div data-role="page" data-title="欢迎" class="jqm-demos">
   <div role="main" class="ui-content jqm-fullwidth">
     <?php include './column.php';?>
-    <h3 class="ui-bar ui-bar-a ui-corner-all">xxx（名称）</h3>
-    <h1>欢迎回来，(个人信息）</h1>
+    <h1>欢迎回来，XXX</h1>
     <h2>此处为空白的文档，为后期可以自由添加内容的模块（既有图片，也可以是文字）</h2>
     <h3>（既有图片，也可以是文字）</h3>
   </div>
